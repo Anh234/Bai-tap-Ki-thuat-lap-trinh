308ca4a6fc6b3b25b611cf65392a49aa9789eee8
Anh234 <105114660+Anh234@users.noreply.github.com>
2022-05-07 20:53:16 +0700

    Add files via upload

---
+++ 20206117-bai2-muc2-1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Pham Ngoc Anh - 20206117
+
+using System;
+
+namespace _20206117_Bai2_Muc2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
