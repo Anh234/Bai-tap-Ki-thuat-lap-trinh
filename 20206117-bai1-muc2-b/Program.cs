5b7f0e9c82c9892cc1b49228762d09e1d82a0d58
Anh234 <105114660+Anh234@users.noreply.github.com>
2022-05-07 20:50:40 +0700

    Add files via upload

---
+++ 20206117-bai1-muc2-b/Program.cs
@@ -0,0 +1,17 @@
+﻿//Pham Ngoc Anh - 20206117
+
+using System;
+
+namespace _20206117_Bai1_Muc2_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Nhập tên của bạn: ");
+            Console.WriteLine("Chào {0}, mình đến với thế giới lập trình ", Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
